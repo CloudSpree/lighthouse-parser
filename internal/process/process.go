6f17cc83c833afa0c34ebd801bdf9a0854a679ce
Stepan Vrany <stepan@vrany.dev>
2023-06-02 13:38:28 +0200

    chore(refactor): decompose process function

--- internal/process/process.go
+++ internal/process/process.go
@@ -35,38 +35,46 @@ func ProcessReport(result audit.Report, prefix string, commit string) (string, e
 		Commit:   commit,
 	}
 
-	// iterate through audits
+	auditMetrics := processAudits(result, auditScoresMetadata, auditNumericValuesMetadata)
+	categoryMetrics := processCategories(result, categoryScoresMetadata)
+
+	return strings.Join(auditMetrics, "\n") + strings.Join(categoryMetrics, "\n"), nil
+}
+
+func processAudits(r audit.Report, scoreMetadata prometheus.AuditScoreMetadata, numericValueMetadata prometheus.AuditNumericValueMetadata) []string {
 	auditScores := []string{}
 	auditNumericValues := []string{}
 
-	for _, a := range result.Audits {
+	for _, a := range r.Audits {
 		if a.Score != nil {
-			auditScores = append(auditScores, prometheus.AuditScore(a, auditScoresMetadata))
+			auditScores = append(auditScores, prometheus.AuditScore(a, scoreMetadata))
 		}
 
 		if a.NumericValue != nil {
-			auditNumericValues = append(auditNumericValues, prometheus.AuditNumericValue(a, auditNumericValuesMetadata))
+			auditNumericValues = append(auditNumericValues, prometheus.AuditNumericValue(a, numericValueMetadata))
 		}
 	}
 
-	// iterate through categories
-	categoryScores := []string{}
-
-	for _, c := range result.Categories {
-		if c.Score != nil {
-			categoryScores = append(categoryScores, prometheus.CategoryScore(c, categoryScoresMetadata))
-		}
-	}
-
-	// decorate metrics
 	auditScores = append(auditScores, "")
 	auditScores = append([]string{"# TYPE audit_score gauge"}, auditScores...)
 
 	auditNumericValues = append(auditNumericValues, "")
 	auditNumericValues = append([]string{"# TYPE audit_numeric_value gauge"}, auditNumericValues...)
 
+	return append(auditScores, auditNumericValues...)
+}
+
+func processCategories(r audit.Report, metadata prometheus.CategoryScoreMetadata) []string {
+	categoryScores := []string{}
+
+	for _, c := range r.Categories {
+		if c.Score != nil {
+			categoryScores = append(categoryScores, prometheus.CategoryScore(c, metadata))
+		}
+	}
+
 	categoryScores = append(categoryScores, "")
 	categoryScores = append([]string{"# TYPE category_score gauge"}, categoryScores...)
 
-	return strings.Join(auditScores, "\n") + strings.Join(auditNumericValues, "\n") + strings.Join(categoryScores, "\n"), nil
+	return categoryScores
 }
